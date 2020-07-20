08e6aa28922fb7a687afe0280df8f9351bb60d67
Nathan Sawyer <sawyenat000@edmonds15.org>
2020-07-20 22:16:01 +0000

    complete

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-num1 = input()
-num2 = input()
-print(num1+num2)
+num1 = int(input("enter first number"))
+num2 = int(input("enter second number"))
+sum = num1 + num2
+print("Addition is: " + str(sum))
+dif = num1 - num2
+print("Subtraction is: "+ str(dif))
+pro = num1 * num2
+print("Multiplication is: " + str(pro))
+quo = num1 / num2
+print ("Division is: " + str(quo))
